63506374209f2eb39980ac5b61bd72ff022210a4
Pablo <pp-pjimenez@zonar.cl>
2026-09-22 21:06:37 -0300

    se modifico el cors

--- ms-orders/src/main/java/com/nexo/msorders/config/SecurityConfig.java
+++ ms-orders/src/main/java/com/nexo/msorders/config/SecurityConfig.java
@@ -39,7 +39,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:*", "exp://*"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "exp://*",
+                "http://nexo-panel-admin.s3-website-us-east-1.amazonaws.com"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- ms-users/src/main/java/com/nexo/msusers/config/SecurityConfig.java
+++ ms-users/src/main/java/com/nexo/msusers/config/SecurityConfig.java
@@ -42,7 +42,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://192.168.0.9:*", "exp://*"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "http://192.168.0.9:*",
+                "exp://*",
+                "http://nexo-panel-admin.s3-website-us-east-1.amazonaws.com"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
